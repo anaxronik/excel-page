80594f068f53a82f8ad2d25a2b1478d3c673650b
Sergey Komarov <anaxronik@gmail.com>
2020-06-17 13:36:17 +0500

    create 3 event on table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,7 +4,30 @@ import {createTable} from "@/components/table/table.template";
 export class Table extends ExcelComponent {
   static className = "excel__table";
 
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      listeners: ['mousedown', 'mouseup', 'mousemove']
+    });
+  }
+
   toHTML() {
     return createTable(100)
+  }
+
+  onMousedown(event) {
+    this.isMouseDown = true
+    console.log('onMousedown(event)')
+  }
+
+  onMouseup(event) {
+    this.isMouseDown = false
+    console.log('onMouseup(event)')
+  }
+
+  onMousemove(event) {
+    if (this.isMouseDown) {
+      console.log('onMousemove(event) ')
+    }
   }
 }
