4d4e5a4f297950c02a38dfbe9090285e619b25b1
Sergey Komarov <anaxronik@gmail.com>
2020-06-26 19:19:50 +0500

    create store and rootReducer.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import "./scss/index.scss";
-import {Header} from "./components/header/Header";
-import {Toolbar} from "./components/toolbar/Toolbar";
-import {Formula} from "./components/formula/Formula";
-import {Excel} from "./components/excel/Excel";
-import {Table} from "@/components/table/Table";
+import "./scss/index.scss"
+import { Header } from "./components/header/Header"
+import { Toolbar } from "./components/toolbar/Toolbar"
+import { Formula } from "./components/formula/Formula"
+import { Excel } from "./components/excel/Excel"
+import { Table } from "@/components/table/Table"
+import { createStore } from "@core/createStore"
+import { rootReducer } from "@/redux/rootReducer"
+
+const store = createStore(rootReducer())
 
 const excel = new Excel("#app", {
   components: [Header, Toolbar, Formula, Table],
-});
+  store,
+})
 
-
-excel.render();
+excel.render()
